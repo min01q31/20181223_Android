79f5fbfa85d967ce4ee2ccaf9855acb0bb023f49
wosad878 <wosad878@naver.com>
2018-12-23 12:25:33 +0900

    Initial commit

---
+++ app/src/main/java/com/iu/a20181223_android/MainActivity.java
@@ -0,0 +1,54 @@
+package com.iu.a20181223_android;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends BaseActivity {
+
+    private android.widget.Button logoutBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bindViews();
+        setupEvents();
+        setValues();
+
+    }
+
+    @Override
+    public void setupEvents() {
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(mContext);
+                alert.setTitle("로그아웃 확인");
+                alert.setMessage("정말 로그아웃 하시겠어요?");
+                alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(mContext, "로그아웃 되었습니다.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                alert.setNegativeButton("취소",null);
+                alert.show();
+            }
+        });
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+        this.logoutBtn = (Button) findViewById(R.id.logoutBtn);
+    }
+}
